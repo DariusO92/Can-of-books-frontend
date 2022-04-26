d2799a51ba32d1ecc0f2052b678f0abc9e4d6e35
Darius <darius.owens.j@gmail.com>
2022-04-25 19:06:41 -0700

    finished lab11

--- src/About.js
+++ src/About.js
@@ -4,7 +4,15 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+      <p>Darius</p>
+      <p>Liesl</p>
+      <p>Gina</p>
+      </>
+    )
+
+    
   }
 };
 
